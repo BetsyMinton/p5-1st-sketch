a047eb572d6d27d72a1facc9b969eca26e0dc01b
Betsy <emm2305@tc.columbia.edu>
2019-07-15 11:21:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# p5-1st-sketch
+ 
---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(480, 120);
+}
+
+function draw() {
+background(204);
+fill(50,155,150);
+strokeWeight(1);
+beginShape();
+vertex(30,70);
+vertex(150,90);
+vertex(90,75);
+vertex(40,30);
+endShape(CLOSE);
+}
