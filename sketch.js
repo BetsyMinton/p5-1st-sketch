b7ef74a7636d891bc66d2009338a9538fe42e192
Betsy <emm2305@tc.columbia.edu>
2019-07-15 20:05:19 -0500

    Update sketch.js
    face

--- sketch.js
+++ sketch.js
@@ -1,15 +1,43 @@
 function setup() {
-  createCanvas(480, 120);
+  createCanvas(720, 480);
+  strokeWeight(2);
+  ellipseMode(RADIUS);
 }
 
 function draw() {
 background(204);
-fill(50,155,150);
-strokeWeight(1);
+//ears
+fill(255);
+ellipse(240,130,8,15);
+fill(255);
+ellipse(320,130,8,15);
+//face
+fill(86);
+ellipse(280,130,40,40);
+//eyes
+fill(255);
+ellipse(295,115,10,10);
+fill(0);
+ellipse(295,115,2,2);
+fill(255);
+ellipse(265,115,10,10);
+fill(0);
+ellipse(265,115,2,2);
+//nose
+fill(255);
+ellipse(280,132,8,10);
+//mouth
+fill(255);
 beginShape();
-vertex(30,70);
-vertex(150,90);
-vertex(90,75);
-vertex(40,30);
+vertex(295,148);
+vertex(290,150);
+vertex(285,153);
+vertex(280,155);
+vertex(275,153);
+vertex(270,150);
+vertex(265,148);
+vertex(273,157);
+vertex(280,160);
+vertex(287,157);
 endShape(CLOSE);
 }
