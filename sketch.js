04f3e17f218c1c9312a22974c0086288a59200d2
Betsy <emm2305@tc.columbia.edu>
2019-07-28 18:43:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ function draw() {
 middle = width/2;
 image(img,0,0);
 stroke(0);
+fill(CDA184);
 //legs
 strokeWeight(1);
 fill(255);
@@ -41,10 +42,10 @@ ellipse(middle+15,eyeHeight,10,10);
 fill(0);
 ellipse(middle+15,eyeHeight,2,2);
 //nose
-fill(255);
+fill(CDA184);
 ellipse(width/2,earHeight,8,10);
 //mouth
-fill(255);
+fill(CA6949);
 beginShape();
 vertex(middle+15,148);
 vertex(middle+10,150);
